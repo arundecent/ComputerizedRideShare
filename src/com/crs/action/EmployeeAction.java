aa490cada0d2c751c7430f173ffbd6170ff4dde6
smilev <smilev2@uic.edu>
2013-04-13 16:03:26 -0500

    Made modifyEmployeeDetails modeldriven

--- src/com/crs/action/EmployeeAction.java
+++ src/com/crs/action/EmployeeAction.java
@@ -1,13 +1,16 @@
 package com.crs.action;
 
 import org.apache.commons.lang.StringUtils;
+import com.crs.model.*;
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
 
-public class EmployeeAction extends ActionSupport {
-
+public class EmployeeAction extends ActionSupport implements ModelDriven<EmployeeForm> {
+	private EmployeeForm employee = new EmployeeForm();
 
 	public String execute() {
+
 		return SUCCESS;
 	}
 	
@@ -15,6 +18,12 @@ public class EmployeeAction extends ActionSupport {
 		System.out.println("hi");
 		
 	
+	}
+
+	@Override
+	public EmployeeForm getModel() {
+		// TODO Auto-generated method stub
+		return employee;
 	}
 
 }

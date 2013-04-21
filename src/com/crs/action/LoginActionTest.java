4d0b9c984837d421ccb141d30b8015ae982e89fc
vishakarajeev <rvisha2@uic.edu>
2013-04-21 15:26:49 -0500

    stubs for loginaction.java

--- src/com/crs/action/LoginActionTest.java
+++ src/com/crs/action/LoginActionTest.java
@@ -1,94 +1,38 @@
 package com.crs.action;
 
 import static org.junit.Assert.*;
-
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
+
 
 public class LoginActionTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Test
-	public void testLoginAction() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetDao() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetDao() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetCarPoolGroup() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetCarPoolGroup() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetMemberList() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetMemberList() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetEmployee() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetEmployee() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetCarPoolMember() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetCarPoolMember() {
-		fail("Not yet implemented");
-	}
-
-	@Test
+		@Test
 	public void testLogin() {
-		fail("Not yet implemented");
+			String check;
+			LoginAction tester= new LoginAction();
+			check = tester.login();
+			assertTrue("login successful", check=="SUCCESS");
+			assertFalse("not satisfied", check=="ERROR");
 	}
 
 	@Test
 	public void testRegisterNewUser() {
-		fail("Not yet implemented");
+		String check;
+		LoginAction tester= new LoginAction();
+		check = tester.registerNewUser();
+		assertTrue("set carpool member and group", check=="SUCCESS");
+		
 	}
 
-	@Test
-	public void testGetModel() {
-		fail("Not yet implemented");
-	}
+	
 
 	@Test
 	public void testModifyEmployeeDetails() {
-		fail("Not yet implemented");
+		String check;
+		LoginAction tester = new LoginAction();
+		check= tester.modifyEmployeeDetails();
+		assertTrue("page to modify details", check=="SUCCESS");
 	}
 
 }

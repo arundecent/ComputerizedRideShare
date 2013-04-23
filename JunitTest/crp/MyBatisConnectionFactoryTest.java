d04a2319cb00353d09befea44d603982358e251a
mohancodehub <mkarun2@uic.edu>
2013-04-23 16:29:53 -0700

    Database Connection Test Commited

--- JunitTest/crp/MyBatisConnectionFactoryTest.java
+++ JunitTest/crp/MyBatisConnectionFactoryTest.java
@@ -1,57 +1,37 @@
 package crp;
-import java.io.Reader;
+
+import static org.junit.Assert.assertTrue;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
 
 import com.crs.dao.MyBatisConnectionFactory;
 /**
  * This class contains all the test cases to check the
  * methods in the MyBatisConnectionFactoryTest.java
- * @author Rajeev
+ * @author Rajeev / Mohan
  *
  */
 
-import static org.junit.Assert.*;
-
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.junit.Test;
-
 public class MyBatisConnectionFactoryTest {
-// To check if the there is no connection with database from Batis
+
+	/**
+	 * This test is to ensure that there is 
+	 * an active database connection
+	 */
 	@Test
-	public void testGetSqlSessionFactory() throws Exception {
-		try
-		{
-		MyBatisConnectionFactory bat = new MyBatisConnectionFactory();
+	public void testGetSqlSessionFactory(){
 		SqlSessionFactory sqlSessionFactory = null;
-		if(sqlSessionFactory == null)
-		{
-			assertTrue("Problem with connection",true);
-		}else{
-			assertTrue("Active connection",false);
-		}
-		}
-		catch (Exception e) {
-			// TODO: handle exception
-		}
-	}
-// Check whether connection has established or not
-	public void testGetSqlSessionFactoryFailed() throws Exception {
-		try
-		{
-			String resource = "SqlMapConfig.xml";
-			Reader reader = Resources.getResourceAsReader(resource);
-			MyBatisConnectionFactory batt = new MyBatisConnectionFactory();
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-			if(sqlSessionFactory == null)
-			{
-				assertTrue("Conection Error",false);
-			}else{
-				assertTrue("Conection Active",true);
+		try {
+			sqlSessionFactory = MyBatisConnectionFactory.getSqlSessionFactory();
+			boolean test = (sqlSessionFactory == null);
+			if (test) {
+				assertTrue("No database connection", false);
+			} else {
+				assertTrue("Active connection", true);
 			}
-		}
-		catch (Exception e) {
-			// TODO: handle exception
+		} catch (Exception e) {
+			assertTrue("No database connection", false);
 		}
 	}
 }
